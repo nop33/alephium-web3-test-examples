f1cb8e7a20fe876448e39fe32a3f9746bab6115c
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-05-13 11:14:36 +0200

    Initial commit

---
+++ node/test.js
@@ -0,0 +1,19 @@
+const { NodeProvider } = require("@alephium/web3");
+
+async function main() {
+  // Create a NodeProvider instance
+  const provider = new NodeProvider("https://node.mainnet.alephium.org");
+
+  // Test address (you can replace this with any valid Alephium address)
+  const testAddress = "1DZiFFX6fnSHuLnnmtBMUWeELWvnhRudYfzb17HYuV9aW";
+
+  try {
+    // Query the balance
+    const balance = await provider.addresses.getAddressesAddressBalance(testAddress);
+    console.log("Balance:", balance);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+main();
